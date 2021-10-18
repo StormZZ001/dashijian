0ff9b593374808665d6d4855b7132b6df4cd771d
StormZZ001 <976183601@qq.com>
2021-10-18 20:46:46 +0800

    10.调用接口发起登录的请求

--- assets/js/login.js
+++ assets/js/login.js
@@ -12,6 +12,7 @@ $(function () {
 
   let form = layui.form;
   let layer = layui.layer;
+  let url = 'http://api-breakingnews-web.itheima.net'
   form.verify({
     // 自定义了一个叫做 pwd 校验规则
     pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
@@ -34,10 +35,9 @@ $(function () {
     e.preventDefault();
     var username = $("#form_reg [name=username]").val();
     var password = $("#form_reg [name=password]").val();
-    var repassword = $("#form_reg [name=repassword]").val();
     //2.调用ajax的post请求
     $.post(
-      "http://api-breakingnews-web.itheima.net/api/reguser",
+      url+'/api/reguser',
       {
         username,
         password,
@@ -54,4 +54,25 @@ $(function () {
       }
     );
   });
+  // 监听登录表单的提交事件
+  $('#form_log').submit(function(e){
+    //1.阻止表单的默认提交行为
+    e.preventDefault()
+    $.ajax({
+      url: url+'/api/login',
+      method:'POST',
+      //快速获取表单中的数据
+      data:$(this).serialize(),
+      success: function (res) {
+        if(res.status !== 0){
+          return layer.msg(res.message)
+        }
+        // console.log(res.token)
+        layer.msg('登录成功')
+        // 将登录成功得到的字符串保留到localStorage中去
+        localStorage.setItem('token',res.token)
+        location.href='/index.html'
+      }
+    });
+  })
 });
