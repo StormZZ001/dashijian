c01127fa4b33b02f87de13f8280d4851dc3e123d
StormZZ001 <976183601@qq.com>
2021-10-21 22:35:52 +0800

    20.后台主页 -- 渲染用户头像以及用户的名称

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,12 +10,32 @@ function getUserInfo() {
         url:'/my/userinfo',
         // headers 就是请求头配置对象
         headers:{
-            Authorization:localStorage.getItem('token')
+            Authorization:localStorage.getItem('token') || ''
         },
         success: function (res) {
             if(res.status !== 0){
                 return layer.msg('获取用户信息失败')
             }
+            //调用renderAvatar渲染用户的头像
+            renderAvatar(res.data)
         }
     });
+}
+//渲染用户的头像
+function renderAvatar(user){
+    //1.获取用户的名称
+    var name = user.username
+    //2.设置获取的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;'+name)
+    //3.按需渲染用户的头像
+    if(user.user_pic !== null){
+        //3.1渲染图片头像
+        $('.layui-nav-img').attr('src',user.user_pic)
+        $('.text-avatar').hide()
+    }else{
+        //3.2渲染文本头像
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
 }
