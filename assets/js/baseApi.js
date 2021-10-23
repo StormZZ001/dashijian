6ca21be45039d969dfe1d5aaa89fb6026a51e5d0
StormZZ001 <976183601@qq.com>
2021-10-23 18:34:13 +0800

    21.为有权限的接口设置heders请求头

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -4,7 +4,14 @@
  * 会先调用 ajaxPrefilter 这个函数
  * 在这个函数中,可以拿到我们给Ajax提供的配置对象
  */
-$.ajaxPrefilter(function (options) {  
-    // console.log(options.url)
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
-})
+$.ajaxPrefilter(function (options) {
+  // console.log(options.url)
+  options.url = "http://api-breakingnews-web.itheima.net" + options.url;
+
+  //统一为有权限的接口,设置 headers 请求头
+  if (options.url.indexOf("/my") !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem("token") || "",
+    };
+  }
+});
